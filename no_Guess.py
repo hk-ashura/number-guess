86e069291e0e9e3d2894a280ead6de72d19e180e
hrishi khawas <hrishikhawas1234@gmail.com>
2026-06-03 16:28:28 +0530

    code

--- no_Guess.py
+++ no_Guess.py
@@ -1,5 +1,24 @@
 import random 
 
+x = random.randint(1,100) 
+
 while True:
-    x = random.randint(1,999999)
-    print("guess ")
+    
+    try:
+        y=int(input("guess the number btw 1 to 100\t"))
+        if not 0<y<101:
+            print("invalid input ")
+        elif x == y :
+            print("correct ")
+            break;
+        elif x<y :
+            print("too high ")
+        elif x> y :
+            print("too low ")
+    except  ValueError :
+        print("enter a valid input")
+
+    
+
+
+    
